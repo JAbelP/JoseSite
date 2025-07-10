997b8134d293c7796448f1ea814bb71290361b9f
Abel P <abelpinales97@gmail.com>
2025-07-10 19:42:31 -0400

    added google Tag - TEST

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata = {
   description: "Estación de radio cristiana hispana con música, programas y contenido inspirador",
 }
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZL2X585HW4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZL2X585HW4');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
