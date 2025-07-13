b7ceb41cb77c8f8445536b7dcdb0b475d1a33830
Abel P <abelpinales97@gmail.com>
2025-07-13 00:26:13 -0400

    debug mode true

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,39 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/app/components/theme-provider"
-import Script from "next/script"
-import Analytics from "./components/analytics"
-import { Suspense } from "react"
+import type React from "react";
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/app/components/theme-provider";
+import Script from "next/script";
+import Analytics from "./components/analytics";
+import { Suspense } from "react";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Radio Alaba a Dios - En Todo Tiempo Alaba a Dios ",
-  description: "Estación de radio cristiana hispana con música, programas y contenido inspirador",
-}
-
+  description:
+    "Estación de radio cristiana hispana con música, programas y contenido inspirador",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-         {/* Load gtag.js */}
-      <Script
+        {/* Load gtag.js */}
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-6GMMNJNS9D"
           strategy="afterInteractive"
         />
         <Script id="gtag-init" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-6GMMNJNS9D');
-          `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-6GMMNJNS9D', { debug_mode: true });
+  `}
         </Script>
       </head>
       <body className={inter.className}>
@@ -41,10 +41,15 @@ export default function RootLayout({
         <Suspense>
           <Analytics />
         </Suspense>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
