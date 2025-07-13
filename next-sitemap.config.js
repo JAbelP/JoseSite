41579b50e6502ed0bf0f09355de81fc123d38f72
Abel P <abelpinales97@gmail.com>
2025-07-13 01:08:01 -0400

    SiteMap done!

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://yourdomain.com', // Change to your actual domain
+  generateRobotsTxt: true,           // Generate robots.txt file
+  generateIndexSitemap: false,        // Generate sitemap-index.xml
+};
